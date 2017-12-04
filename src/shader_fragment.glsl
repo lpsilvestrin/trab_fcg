b8b727c480f0c1ee220462a08bed62b45e83bc1f
lpsilvestrin <luisp.silvestrin@gmail.com>
2017-12-03 22:18:21 -0200

    testing textures, currently not working

--- src/shader_fragment.glsl
+++ src/shader_fragment.glsl
@@ -1,6 +1,7 @@
 #version 330 core
 
 #define M_PI 3.14159265358979323846
+#define M_PI_2 1.57079632679489661923
 
 // Atributos de fragmentos recebidos como entrada ("in") pelo Fragment Shader.
 // Neste exemplo, este atributo foi gerado pelo rasterizador como a
@@ -8,6 +9,11 @@
 // "main.cpp".
 in vec4 position_world;
 in vec4 normal;
+
+// Posição do vértice atual no sistema de coordenadas local do modelo.
+in vec4 position_model;
+// Coordenadas de textura obtidas do arquivo OBJ (se existirem!)
+in vec2 texcoords;
 
 // Matrizes computadas no código C++ e enviadas para a GPU
 uniform mat4 model;
@@ -19,6 +25,15 @@ uniform mat4 projection;
 #define BUNNY  1
 #define PLANE  2
 uniform int object_id;
+
+// Parâmetros da axis-aligned bounding box (AABB) do modelo
+uniform vec4 bbox_min;
+uniform vec4 bbox_max;
+
+// Variáveis para acesso das imagens de textura
+uniform sampler2D TextureImage0;
+uniform sampler2D TextureImage1;
+uniform sampler2D TextureImage2;
 
 // O valor de saída ("out") de um Fragment Shader é a cor final do fragmento.
 out vec3 color;
@@ -53,21 +68,35 @@ void main()
 
     // Vetor que define o sentido da reflexão especular ideal.
     vec4 r = -l + 2*n*dot(n,l); // PREENCHA AQUI o vetor de reflexão especular ideal
+    
+// Coordenadas de textura U e V
+    float U = 0.0;
+    float V = 0.0;
 
     // Parâmetros que definem as propriedades espectrais da superfície
     vec3 Kd; // Refletância difusa
     vec3 Ks; // Refletância especular
     vec3 Ka; // Refletância ambiente
     float q; // Expoente especular para o modelo de iluminação de Phong
+    vec3 Kd0 = vec3(0.0,0.0,0.0);
 
     if ( object_id == SPHERE )
     {
         // PREENCHA AQUI
         // Propriedades espectrais da esfera
-        Kd = vec3(0.8,0.4,0.08);
-        Ks = vec3(0.0,0.0,0.0);
-        Ka = vec3(0.4,0.2,0.04);
+        Kd = vec3(0.0,0.0,0.8);
+        Ks = vec3(0.0,0.0,0.8);
+        Ka = vec3(0.0,0.0,0.2);
         q = 1.0;
+        
+		vec4 bbox_center = (bbox_min + bbox_max) / 2.0;
+        float len   = length(position_model - bbox_center);
+        float theta = atan(position_model[0], position_model[2]);
+        float phi   = asin(position_model[1]/len);
+
+        U = (theta + M_PI)/(2*M_PI);
+        V = (phi + M_PI_2)/M_PI;
+		Kd0 = texture(TextureImage0, vec2(U,V)).rgb;
     }
     else if ( object_id == BUNNY )
     {
@@ -80,11 +109,15 @@ void main()
     }
     else if ( object_id == PLANE )
     {
+		U = texcoords.x;
+		V = texcoords.y;
+
         // PREENCHA AQUI
         // Propriedades espectrais do plano
         Kd = vec3(0.2,0.2,0.2);
         Ks = vec3(0.3,0.3,0.3);
         Ka = vec3(0.0,0.0,0.0);
+		Kd0 = texture(TextureImage0, vec2(U,V)).rgb;
         q = 20.0;
     }
     else // Objeto desconhecido = preto
@@ -111,9 +144,9 @@ void main()
 	// Termo difuso utilizando a lei dos cossenos de Lambert
 	vec3 lambert_diffuse_term = max(0,dot(n,l))*vec3(Kd[0]*I[0],Kd[1]*I[1],Kd[2]*I[2]); // PREENCHA AQUI o termo difuso de Lambert
 
-    // Cor final do fragmento calculada com uma combinação dos termos difuso,
-    // especular, e ambiente. Veja slide 134 do documento "Aula_17_e_18_Modelos_de_Iluminacao.pdf".
-    color = lambert_diffuse_term + ambient_term + phong_specular_term;
+
+    // Cor final utilizando textura
+	color = Kd0 + lambert_diffuse_term + ambient_term + phong_specular_term;
     
 // Cor final com correção gamma, considerando monitor sRGB.
     // Veja https://en.wikipedia.org/w/index.php?title=Gamma_correction&oldid=751281772#Windows.2C_Mac.2C_sRGB_and_TV.2Fvideo_standard_gammas
